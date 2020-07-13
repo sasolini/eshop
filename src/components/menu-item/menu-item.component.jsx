64b7dc4133c72c82f5fbbdff59b4427d998770e3
Saso Hribernik <saso.hribernik@hribis.com>
2020-07-13 09:13:07 +0100

    Finished styling main directory and menu components

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imgUrl, sectionSize }) => (
+  <div className={`${sectionSize} menu-item`}>
+    <div
+      className='background-image'
+      style={{ backgroundImage: `url(${imgUrl})` }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
