a22f9be2a9532641e0a9aac63d2f4ca242b49bdd
Saso Hribernik <saso.hribernik@hribis.com>
2020-07-15 19:27:36 +0100

    added state persistance

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import * as serviceWorker from './serviceWorker';
 
@@ -16,7 +17,9 @@ ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
