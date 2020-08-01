1fc6ed633f634e7d152b1ed18929aefcf1e6123c
Saso Hribernik <saso.hribernik@hribis.com>
2020-08-01 21:20:42 +0100

    moved app to client folder, added express server to handle backend stripe payment

---
+++ client/build/precache-manifest.6218846864b2ef51cf984d14c05290c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcd8847d2e46170ec7dd683e4f46fa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2992200bb3e7d58dd1a4",
+    "url": "/static/css/main.8d3396b6.chunk.css"
+  },
+  {
+    "revision": "a4bc3b74094c1ad8cdca",
+    "url": "/static/js/2.a1ec98a7.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.a1ec98a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2992200bb3e7d58dd1a4",
+    "url": "/static/js/main.b95f7137.chunk.js"
+  },
+  {
+    "revision": "884421c92f43d3cab84a",
+    "url": "/static/js/runtime-main.b7c8601d.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51H9WP9B9mjj8BRnk6Rz25aYKxooFQaPB691SOFPQgWegonXUwH92qvFylC4kszcoFeAPekjMmzGhuEa3FW26gqqk00EEvkCKNO';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then(response => {
+        alert('Payment successful');
+      })
+      .catch(error => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please make sure you use the provided credit card.'
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='eShop Demo Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/Cuz.svg'
+      description={`Your total is £${price}`}
+      amount={priceForStripe}
+      currency='GBP'
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
