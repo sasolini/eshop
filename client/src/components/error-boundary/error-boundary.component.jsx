5c5a3aca5a0c7bb9598066af601795396004eb97
Saso Hribernik <saso.hribernik@hribis.com>
2020-08-05 14:25:21 +0100

    added performance optimizations

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {
+  ErrorImageOverlay,
+  ErrorImageContainer,
+  ErrorImageText,
+} from './error-boundary.styles';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasErrored: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasErrored) {
+      return (
+        <ErrorImageOverlay>
+          <ErrorImageContainer imageUrl='https://i.imgur.com/QIxIKBH.png' />
+          <ErrorImageText>This Page is a Ghost</ErrorImageText>
+        </ErrorImageOverlay>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
